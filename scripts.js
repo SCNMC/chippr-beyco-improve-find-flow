954b600f81b12c22dbf88d7d25ef3310b402f4da
Shauri Maigua <90189750+SCNMC@users.noreply.github.com>
2021-12-19 16:11:29 +0100

    first version finished

---
+++ scripts.js
@@ -0,0 +1,15 @@
+function openNav() {
+    document.getElementById("myFilterbutton").style.width = "250px";
+   
+  }
+  
+  function closeNav() {
+    document.getElementById("myFilterbutton").style.width = "0";
+    document.body.style.backgroundColor = "white";
+  }
+
+ 
+
+
+
+
